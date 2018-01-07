dc840e8c1b02e05e81584d27ce620971deea3975
VvCheng <1573087904@qq.com>
2018-01-07 23:41:01 +0800

    增加学生的service层

---
+++ src/main/java/com/system/service/StudentService.java
@@ -0,0 +1,43 @@
+package com.system.service;
+
+import com.system.po.Student;
+import com.system.po.StudentCustom;
+
+import java.util.List;
+
+/**
+ * Student学生的Service层
+ */
+public interface StudentService {
+
+    //根据id个更新学生信息
+    void updataById(Integer id, StudentCustom studentCustom) throws Exception;
+
+    //根据id删除学生信息
+    void removeById(Integer id) throws Exception;
+
+    //获取分页查询学生信息
+    List<StudentCustom> findByPaging(Integer toPageNo) throws Exception;
+
+    //保存学生信息
+    Boolean save(StudentCustom studentCustoms) throws Exception;
+
+    //获取学生总数
+    int getCountStudent() throws Exception;
+
+    //根据id获取学生信息
+    StudentCustom findById(Integer id) throws Exception;
+
+    //根据名字模糊查询
+    List<StudentCustom> findByName(String name) throws Exception;
+
+    // 一对多查询，查询该学生的选课信息
+    StudentCustom findStudentAndSelectCourseListByName(String name) throws Exception;
+
+
+    //获取学生信息
+    Student profile(int userid) throws Exception;
+
+    //更新学生信息
+    int profileUpdate(Student student) throws Exception;
+}
